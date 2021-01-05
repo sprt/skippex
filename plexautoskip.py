fc8d880d1df8ec8edcb1a71a0bb4178798e7494f
sprt <hellosprt@gmail.com>
2021-01-05 12:41:03 +0100

    Use HTTP polling instead of WebSockets

--- plexautoskip.py
+++ plexautoskip.py
@@ -1,21 +1,24 @@
 from __future__ import annotations
 
+from abc import ABC, abstractmethod
 import argparse
 from collections.abc import Callable, Iterator, MutableMapping
 import configparser
+from datetime import datetime, timedelta
 from functools import partial
 from io import TextIOBase
 import json
 import logging
 from pathlib import Path
 from time import sleep
-from typing import Any, Literal, NamedTuple, Optional, TypedDict
+from typing import Any, Literal, NamedTuple, Optional, TypedDict, cast
 from urllib.parse import urlencode
 import uuid
 import sys
 import webbrowser
 
 from plexapi.alert import AlertListener as PlexAlertListener
+from plexapi.base import Playable
 from plexapi.client import PlexClient
 from plexapi.myplex import MyPlexAccount
 from plexapi.server import PlexServer
@@ -28,12 +31,12 @@ logger = logging.getLogger('plexautoskip')
 _APP_NAME = 'Plex Auto-Skip'
 _PLEXAUTOSKIP_INI = Path('.plexautoskip.ini')
 
+SessionKey = str
+Store = MutableMapping[str, Any]
+
 
 def _print_stderr(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
-
-
-Store = MutableMapping[str, Any]
 
 
 class INIStore(Store):
@@ -208,78 +211,163 @@ class PlaybackNotification(TypedDict):
     state: Literal['buffering', 'playing', 'paused']
 
 
-class NoMatchingClientError(Exception):
-    """Raised when a session (episode being watched) has no matching client."""
+class SessionListener(ABC):
+    def filter_session(self, session: Playable) -> bool:
+        """See the docstrings for the callback methods."""
+        return True
+
+    @abstractmethod
+    def on_session_activity(self, session: Playable):
+        """Called iff filter_session(session) returns True."""
+        pass
+
+    @abstractmethod
+    def on_session_removal(self, session: Playable):
+        """Called iff on_session_activity(session) was called at some point."""
+        pass
 
 
-class NoMatchingSessionError(Exception):
-    """Raised when a notification has no matching session."""
+class Seekable(ABC):
+    @abstractmethod
+    def seek(self, offset_ms: int):
+        pass
 
 
-class AlertCallback:
-    def __init__(self, server: PlexServer, account: MyPlexAccount, db: Database):
+class SeekablePlexClient(Seekable):
+    def __init__(self, client: PlexClient):
+        self._client = client
+
+    def seek(self, offset_ms: int):
+        self._client.seekTo(offset_ms)
+
+
+class NoSeekableFoundError(Exception):
+    pass
+
+
+class SeekableProvider(ABC):
+    @abstractmethod
+    def provide_seekable(self, session: Playable) -> Seekable:
+        """Raises NoSeekableFoundError if no Seekable could be found."""
+        pass
+
+
+class PlexSeekableProvider(SeekableProvider):
+    def __init__(self, server: PlexServer):
         self._server = server
-        self._account = account
-        self._db = db
 
-    def _match_session(self, notification: PlaybackNotification) -> Episode:
-        for session in self._server.sessions():
-            is_match = (
-                str(session.sessionKey) == notification['sessionKey']
-                and isinstance(session, Episode)
-                and session.hasIntroMarker
-            )
-            if is_match:
-                return session
-        raise NoMatchingSessionError
-
-    def _match_client(self, session: Episode) -> PlexClient:
-        # Search server.clients() using machineIdentifier. session.players()
-        # doesn't work because the PlexClients don't have a baseurl, thus
-        # can't make requests.
-
-        # XXX: Does the session ever not contain exactly 1 player?
+    def provide_seekable(self, session: Playable) -> Seekable:
         sess_machine_id = session.players[0].machineIdentifier
-        # NOTE: Have to "advertize as player" in order to be seen by clients().
+        # NOTE: Have to "advertize as player" in order to be considered a client by Plex.
+        client: PlexClient
         for client in self._server.clients():
             if client.machineIdentifier == sess_machine_id:
-                return client
-        raise NoMatchingClientError
+                return SeekablePlexClient(client)
+        raise NoSeekableFoundError
 
-    def _handle_notification(self, notification: PlaybackNotification):
-        if notification['state'] != 'playing':
-            return
 
+class ChromecastSeekableProvider(SeekableProvider):
+    pass
+
+
+class AutoSkipper(SessionListener):
+    def __init__(self, seekable_provider: SeekableProvider):
+        super().__init__()
+        self._skipped: set[SessionKey] = set()
+        self._sp = seekable_provider
+
+    def filter_session(self, session: Playable) -> bool:
+        if not isinstance(session, Episode):
+            # Only TV shows have intro markers, other media don't interest us.
+            logger.debug('Ignored; not an episode')
+            return False
+
+        # XXX: From my testing, a session always has exactly one player.
+        player: PlexClient = session.players[0]
+        player_state: Literal['buffering', 'paused', 'playing'] = player.state
+
+        if player_state != 'playing':
+            logger.debug(f'Ignored; state is "{player_state}" instead of "playing"')
+            return False
+
+        # Preserve the view offset.
+        # See https://github.com/pkkid/python-plexapi/issues/638.
+        # Potential alernative: https://stackoverflow.com/a/1445289/407054.
         try:
-            session = self._match_session(notification)
-        except NoMatchingSessionError:
-            # That's okay. Could simply be that the user is watching a movie,
-            # or that the episode has no intro markers.
+            view_offset = session.viewOffset
+            if not session.hasIntroMarker:
+                logger.debug(f'Ignored; has no intro marker')
+                return False
+        finally:
+            session.viewOffset = view_offset
+
+        return True
+
+    def on_session_activity(self, session: Playable):
+        session_key = str(session.sessionKey)
+        if session_key in self._skipped:
+            logger.debug('Ignored; already skipped during this session')
             return
 
-        client = self._match_client(session)
-        view_offset = notification['viewOffset']
+        session = cast(Episode, session)  # Safe thanks to self.filter_session().
+        view_offset = session.viewOffset
         intro_marker = next(m for m in session.markers if m.type == 'intro')
 
-        print(f'{notification["sessionKey"]=} {notification["viewOffset"]=} {notification["state"]=}')
-        print(f'{session=}')
-        print(f'{client=}')
-        print(f'{intro_marker=}')
+        logger.debug(f'{session.sessionKey=}')
+        logger.debug(f'{view_offset=}')
+        logger.debug(f'{intro_marker=}')
 
         if intro_marker.start <= view_offset and view_offset < intro_marker.end:
-            client.seekTo(intro_marker.end)
-            print(f'Seeked from {view_offset} to {intro_marker.end}')
+            seekable = self._sp.provide_seekable(session)
+            seekable.seek(intro_marker.end)
+            self._skipped.add(session_key)
+            logger.debug(f'Skipped; seeked from {view_offset} to {intro_marker.end}')
         else:
-            print('Did not seek')
+            logger.debug('Did not skip; not viewing intro')
 
-        print()
+        logger.debug('-----')
 
-    def __call__(self, alert: dict[str, Any]):
-        if alert['type'] == 'playing':
-            # Never seen a case where the alert doesn't contain exactly
-            # 1 notification, but let's loop over the list out of caution.
-            for notification in alert['PlaySessionStateNotification']:
-                self._handle_notification(notification)
+    def on_session_removal(self, session: Playable):
+        self._skipped.discard(str(session.sessionKey))
+
+
+class SessionDispatcher:
+    def __init__(self, listener: SessionListener, removal_timeout_sec: int = 20):
+        # Plex's session WebSocket announces every 10 second, so we
+        # conservatively set the default removal_timeout_sec to 20 seconds.
+        self._listener = listener
+        self._removal_timeout_sec = removal_timeout_sec
+        self._last_active: dict[str, tuple[datetime, Playable]] = {}
+
+    def dispatch(self, session: Playable):
+        if self._listener.filter_session(session):
+            self._listener.on_session_activity(session)
+            key = str(session.sessionKey)
+            self._last_active[key] = (datetime.now(), session)
+
+        # Remove inactive sessions.
+        timeout_ago = datetime.now() - timedelta(seconds=self._removal_timeout_sec)
+        inactive = (
+            (key, session) for key, (last_active, session) in self._last_active.items()
+            if last_active <= timeout_ago
+        )
+        for inactive_key, inactive_session in inactive:
+            self._listener.on_session_removal(inactive_session)
+            self._last_active.pop(inactive_key, None)
+
+
+class SessionDiscovery:
+    def __init__(self, server: PlexServer, dispatcher: SessionDispatcher, poll_interval_sec: int = 1):
+        self._server = server
+        self._dispatcher = dispatcher
+        self._poll_interval_sec = poll_interval_sec
+
+    def run(self):
+        while True:
+            session: Playable
+            for session in self._server.sessions():
+                self._dispatcher.dispatch(session)
+            sleep(self._poll_interval_sec)
 
 
 def cmd_default(args: argparse.Namespace, db: Database, app: PlexApplication) -> Optional[int]:
@@ -305,13 +393,13 @@ def cmd_default(args: argparse.Namespace, db: Database, app: PlexApplication) ->
         _print_stderr("Couldn't find a Plex server for this account.")
         return 1
 
-    # TODO: Ensure we try HTTP only if HTTPS fails.
-    server = server_resource.connect()
+    server = server_resource.connect()  # TODO: Ensure we try HTTP only if HTTPS fails.
+    sp = PlexSeekableProvider(server)
+    listener = AutoSkipper(seekable_provider=sp)
+    dispatcher = SessionDispatcher(listener)
 
-    alert_callback = AlertCallback(server, account, db)
-    alert_listener = AlertListener(server, alert_callback)
-    alert_listener.start()
-    alert_listener.join()
+    discovery = SessionDiscovery(server, dispatcher)
+    discovery.run()
 
 
 def main():
