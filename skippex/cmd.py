7a9c06c390ccfbf59133498717d4ecbe669e45b3
sprt <hellosprt@gmail.com>
2021-01-16 22:55:32 +0100

    Different DB/PID paths for dev/prod
    Caused conflicts when the dev paths were the same as the paths for the
    package installed using pipx.

--- skippex/cmd.py
+++ skippex/cmd.py
@@ -1,6 +1,7 @@
 import argparse
 from functools import partial
 import logging
+import os
 import shelve
 import sys
 from typing import Optional
@@ -25,11 +26,16 @@ from .sessions import SessionDiscovery, SessionDispatcher, SessionProvider
 from .stores import Database
 
 
+if os.getenv('SK_DEV', '0') == '1':
+    _DATABASE_PATH = xdg.xdg_data_home() / 'skippex_dev.db'
+    _PID_NAME = 'skippex_dev.pid'
+else:
+    _DATABASE_PATH = xdg.xdg_data_home() / 'skippex.db'
+    _PID_NAME = 'skippex.pid'
+
 _APP_NAME = 'Skippex'
 _APP_ARGV0 = __package__
-_DATABASE_PATH = xdg.xdg_data_home() / 'skippex.db'
 _PID_DIR = xdg.xdg_runtime_dir()
-_PID_NAME = 'skippex.pid'
 _PID_PATH = _PID_DIR / _PID_NAME
 
 
--- tests/test_stores.py
+++ tests/test_stores.py
@@ -1,4 +1,5 @@
-from typing import Tuple
+from pathlib import Path
+import shelve
 
 import pytest
 
@@ -6,23 +7,31 @@ from skippex.stores import Database
 
 
 @pytest.fixture
-def db() -> Database:
-    return Database({})
-
-
-@pytest.fixture
-def two_dbs() -> Tuple[Database, Database]:
-    return Database({}), Database({})
+def db(request, tmp_path: Path) -> Database:
+    if request.param == 'dict':
+        return Database({})
+    elif request.param == 'shelf':
+        shelf = shelve.open(str(tmp_path / 'shelf.db'))
+        return Database(shelf)
+    else:
+        raise ValueError
 
 
 class TestDatabase:
+    @pytest.mark.parametrize('db', ['dict', 'shelf'], indirect=True)
     def test_app_id__has_default(self, db: Database):
         assert db.app_id
 
+    @pytest.mark.parametrize('db', ['dict', 'shelf'], indirect=True)
+    def test_app_id__has_default(self, db: Database):
+        assert db.app_id
+
+    @pytest.mark.parametrize('db', ['dict', 'shelf'], indirect=True)
     def test_app_id__default_persists(self, db: Database):
         default = db.app_id
         assert db.app_id == default
 
-    def test_app_id__default_is_unique(self, two_dbs: Tuple[Database, Database]):
-        db1, db2 = two_dbs
+    def test_app_id__default_is_unique(self):
+        db1 = Database({})
+        db2 = Database({})
         assert db1.app_id != db2.app_id
