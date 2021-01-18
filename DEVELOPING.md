4d4c2b3851e0cb27d1b60f2085584237e21365cb
sprt <hellosprt@gmail.com>
2021-01-18 18:03:53 +0100

    Create release script
    Also remove the extras from pyproject.toml as those seem buggy:
    
    https://github.com/python-poetry/poetry/issues/2073
    https://github.com/python-poetry/poetry/issues/1145

--- DEVELOPING.md
+++ DEVELOPING.md
@@ -9,30 +9,8 @@ $ tox
 
 ## Releasing
 
-Prerequisites:
-
- 1. Ensure we're on branch main (`git branch`).
- 1. Ensure the repo is clean (`git status`).
- 1. Ensure all the tests pass (`tox`).
-
 ```console
-$ poetry version <bump>
-$ git add pyproject.tml && git commit -m "vX.X.X release"
-$ git tag -a vX.X.X -m "vX.X.X release"
-
-$ # Create Docker image.
-$ docker build -t ghcr.io/sprt/skippex:X.X.X .
-$ # Test image.
-$ docker run --rm --network host --entrypoint sh ghcr.io/sprt/skippex:X.X.X -c ". /venv/bin/activate && python -m pytest"
-$ # Tag image with "latest".
-$ docker tag ghcr.io/sprt/skippex:X.X.X ghcr.io/sprt/skippex:latest
-$ # Publish on PYPI.
-$ poetry publish --build
-$ # Publish on GitHub Container Registry.
-$ docker push ghcr.io/sprt/skippex:X.X.X
-$ docker push ghcr.io/sprt/skippex:latest
-$ # Push to git repo.
-$ git push --follow-tags
+$ python release.py
 ```
 
 ## Building a development Docker image
---
+++ release.py
@@ -0,0 +1,185 @@
+import logging
+import logging.config
+import re
+import subprocess
+import sys
+from types import TracebackType
+from typing import List, NamedTuple, Optional, Type, cast
+
+import subprocess_tee
+
+
+# Disable third-party loggers.
+logging.config.dictConfig({
+    'version': 1,
+    'disable_existing_loggers': True,
+})
+
+logger = logging.getLogger(__name__)
+
+# Reference: https://semver.org/
+re_semver = re.compile(r'^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$')
+
+
+class Rollback(Exception):
+    pass
+
+
+class Command(NamedTuple):
+    commit: str
+    rollback: Optional[str]
+    pure: bool
+
+
+class Transaction:
+    def __init__(self) -> None:
+        self._executed: List[Command] = []
+        self.committed: Optional[bool] = None
+
+    def _execute(self, cmd: str) -> subprocess.CompletedProcess:
+        logger.info(f'--> {cmd}')
+        p = subprocess_tee.run(cmd, shell=True)
+        # check=True isn't supported by subprocess_tee.run():
+        # https://github.com/pycontribs/subprocess-tee/issues/26
+        p.check_returncode()
+        return p
+
+    def execute(self, commit: str, rollback: Optional[str] = None, pure: bool = False) -> subprocess.CompletedProcess:
+        if pure and rollback:
+            raise ValueError('cannot both be pure and have a rollback')
+
+        p = self._execute(commit)
+        command = Command(commit=commit, rollback=rollback, pure=pure)
+        self._executed.append(command)
+
+        return p
+
+    def __enter__(self) -> 'Transaction':
+        return self
+
+    def __exit__(
+        self,
+        exc_type: Optional[Type[BaseException]],
+        exc_value: Optional[BaseException],
+        exc_traceback: Optional[TracebackType],
+    ) -> bool:
+        if not exc_type:
+            self.committed = True
+            logger.info('Transaction committed')
+            return True
+
+        if exc_type is subprocess.CalledProcessError:
+            exc_value = cast(subprocess.CalledProcessError, exc_value)
+            logger.warn(f'Command {exc_value.cmd!r} exited with status {exc_value.returncode}')
+        else:
+            logger.warn('Exception raised:', exc_info=True)
+
+        logger.info('Rolling back transaction...')
+        num_rollback_fails = 0
+
+        while self._executed:
+            command = self._executed.pop()
+
+            if command.pure:
+                logger.info(f'Command {command.commit!r} is pure, no rollback')
+            elif not command.rollback:
+                logger.error(f'Command {command.commit!r} has no rollback')
+            else:
+                try:
+                    logger.info(f'Rolling back command {command.commit!r}...')
+                    self._execute(command.rollback)
+                except BaseException as e:
+                    if isinstance(e, subprocess.CalledProcessError):
+                        logger.error(
+                            f'Rollback of command {command.commit!r} failed: '
+                            f'{command.rollback!r} exited with status {e.returncode}'
+                        )
+                    else:
+                        logger.exception(f'Rollback of command {command.commit!r} failed:')
+                    num_rollback_fails += 1
+                else:
+                    logger.info(f'Rollback of command {command.commit!r} succeeded')
+
+        logger.info('Transaction rolled back')
+        if num_rollback_fails:
+            logger.error(f'But failed to rollback {num_rollback_fails} commands')
+
+        self.committed = False
+        return True
+
+
+if __name__ == '__main__':
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format='[%(levelname)-8s] %(message)s',
+    )
+
+    tx = Transaction()
+
+    with tx:
+        # Prerequisites:
+
+        # Ensure we're on the main branch.
+        tx.execute('[ "$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)" = "main" ]', pure=True)
+        # Ensure the repo is clean.
+        tx.execute('[ -z "$(git status --porcelain)" ]', pure=True)
+        # Ensure the tests pass.
+        tx.execute('PY_COLORS=1 tox -- --color=yes', pure=True)
+
+        # Actual release process:
+
+        # Bump the version in pyproject.toml.
+        p_poetry_version = tx.execute(
+            'poetry version "3.0.0"',
+            rollback='git checkout HEAD -- pyproject.toml',
+        )
+
+        version = p_poetry_version.stdout.split()[-1]
+        assert re_semver.match(version), f'not a valid semver: {version}'
+        git_tag = f'v{version}'
+        docker_tag_version = f'ghcr.io/sprt/skippex:{version}'
+        docker_tag_latest = f'ghcr.io/sprt/skippex:latest'
+
+        # Commit pyproject.toml.
+        tx.execute(
+            f'git commit -m "v{version} release" pyproject.toml',
+            rollback='git reset --hard HEAD',
+        )
+
+        # Tag the commit.
+        tx.execute(
+            f'git tag -a {git_tag}',
+            rollback=f'git tag -d {git_tag}',
+        )
+
+        # Create the Docker image.
+        tx.execute(
+            f'docker build -t {docker_tag_version} .',
+            rollback=f'docker rmi {docker_tag_version}',
+        )
+
+        # Test the image.
+        tx.execute(
+            f'docker run --rm --network host --entrypoint sh {docker_tag_version} -c ". /venv/bin/activate && python -m pytest"',
+            pure=True,
+        )
+
+        # Tag the image with "latest".
+        # TODO: Reassign the latest tag to its original image in rollback?
+        tx.execute(
+            f'docker tag {docker_tag_version} {docker_tag_latest}',
+            rollback=f'docker rmi {docker_tag_latest}'
+        )
+
+        # Publish on PyPI.
+        # TODO: Deal with the username/password prompts.
+        tx.execute('poetry publish --build')
+
+        # Publish on GitHub Container Registry.
+        tx.execute(f'docker push {docker_tag_version}')
+        tx.execute(f'docker push {docker_tag_latest}')
+
+        # Push to git repo.
+        tx.execute('git push --follow-tags')
+
+    sys.exit(int(not tx.committed))
